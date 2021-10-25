5003b9c48b405af45e4b4a49f96b5e72210f20e6
Andrey Zak <amzak.only@gmail.com>
2021-10-25 19:35:44 +0300

    Initial version

---
+++ src/dir_walker.rs
@@ -0,0 +1,75 @@
+use std::env;
+use std::path::{Path, PathBuf};
+use std::fs::{self, DirEntry};
+use std::io;
+
+pub struct DirWalker {
+  iter: fs::ReadDir,
+  path_stack: Vec<DirEntry>
+}
+
+impl DirWalker {
+  pub fn new(baseDir: PathBuf) -> Result<DirWalker, io::Error> {
+    let iter = fs::read_dir(baseDir)?;
+    let vec = Vec::new();
+
+    return Ok(DirWalker {
+      path_stack: vec,
+      iter
+    });
+  }
+
+  fn iter_on(&mut self, dir_entry: DirEntry) {
+    let path = dir_entry.path();
+    self.iter = fs::read_dir(path).unwrap();
+  }
+}
+
+impl Iterator for DirWalker {
+  type Item = PathBuf;
+
+  fn next(&mut self) -> Option<PathBuf> {
+    let path = self.iter.next();
+
+    match path {
+      Some(x) => {
+        let result = x.unwrap();
+        let path_buf = result.path();
+        if path_buf.is_dir() {
+          self.path_stack.push(result);
+          return self.next();
+        }
+        return Some(result.path());
+      },
+      None => {
+        let path_from_stack = self.path_stack.pop();
+
+        if let Some(path) = path_from_stack {
+            self.iter_on(path);
+            return self.next();
+        }
+        else {
+          return None;
+        }
+      }
+    }
+  }
+}
+
+#[cfg(test)]
+mod dirWalkerTests {
+  use super::*;
+
+  #[test]
+  fn internal() {
+      let currentDir = env::current_dir().unwrap();
+      println!("{:?}", currentDir);
+      let walkerResult = DirWalker::new(currentDir);
+
+      if let Ok(walker) = walkerResult {
+        for path in walker {
+            println!("{:?}", path);
+        }
+      }
+  }
+}
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+mod dir_walker;
+
+#[derive(Debug, StructOpt)]
+struct Options {
+    #[structopt(short, long)]
+    dry_run: bool,
+}
+
+fn try_read_bom(path: &PathBuf, buffer: &mut [u8]) -> std::io::Result<bool> {
+    let _file = File::open(&path)?.read_exact(buffer)?;
+    Ok(true)
+}
+
+fn main()
+{
+    let options = Options::from_args();
+    let current_dir = env::current_dir().expect("can't get current directory");
+    println!("checking boms in {:?} ...", current_dir);
+
+    let files_list = dir_walker::DirWalker::new(current_dir).unwrap();
+
+    let mut buffer: [u8; 3] = [0;3];
+    let bom: [u8; 3] = [0xef, 0xbb, 0xbf];
+
+    for path in files_list {
+        if let Err(err) = try_read_bom(&path, &mut buffer) {
+            println!("ERROR {:?}", err.to_string());
+            continue;
+        }
+        
+        print!("{:?} ", path);
+
+        let has_bom = buffer.iter().zip(&bom).all(|(first, second)| first == second);
+
+        if has_bom {
+            println!("BOM");
+        }
+        else {
+            println!("no BOM");
+        }
+    }
+}
